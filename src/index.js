30ee20c36a1b65591cd211524650bbf53df955e7
kbjude <kbjude@gmail.com>
2020-09-13 19:20:35 +0300

    Well setup webpark

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 
-function component() {
+  function component() {
     const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
+   // Lodash, currently included via a script, is required for this line to work
+   // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', + ' ' + 'webpack'], ' ');
+    console.log(alert("Whats happening"))
+
     return element;
   }
-  
+
   document.body.appendChild(component());
