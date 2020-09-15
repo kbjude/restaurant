4a3f945d381feb32c4703d5d8d8d18ff331712d0
Jude Kajura <kbjude@gmail.com>
2020-09-15 21:04:10 +0300

    listing the navbar

--- src/outputs.js
+++ src/outputs.js
@@ -4,14 +4,24 @@ import { head } from 'lodash';
 
 const displaylayout = () => {
     const content = document.getElementById('content');
-    const header = changeDom.createHtmlElement({tag: 'header', parentElement: 'content', arrayClassName: ['header']});
-    const navbar = changeDom.createHtmlElement({tag: 'nav', parentElement: 'header', arrayClassName: ['navbar']})
-    const mainsection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['mainsection']});
-    const middlesection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['middlesection','d-flex']});
-    const lowersection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['lowersection']});
-    const footersection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['footersection']});
-    const navlist = changeDom.createHtmlElement({tag: 'ul', parentElement: 'navbar', arrayClassName: ['navlist'] })
-    changeDom.createHtmlElement({ })
+    const header = changeDom.createHtmlElement({tag: 'header', parentElement: content, arrayClassName: ['header']});
+    const navbar = changeDom.createHtmlElement({tag: 'nav', parentElement: header, arrayClassName: ['navbar']})
+    const mainsection = changeDom.createHtmlElement({tag: 'div', parentElement: content, arrayClassName: ['mainsection']});
+    const middlesection = changeDom.createHtmlElement({tag: 'div', parentElement: content, arrayClassName: ['middlesection','d-flex']});
+    const lowersection = changeDom.createHtmlElement({tag: 'div', parentElement: content, arrayClassName: ['lowersection']});
+    const footersection = changeDom.createHtmlElement({tag: 'div', parentElement: content, arrayClassName: ['footersection']});
+    const navlist = changeDom.createHtmlElement({tag: 'ul', parentElement: navbar, arrayClassName: ['navlist'] })
+    changeDom.createHtmlElement({ 
+      tag: 'li', parentElement: navlist, elementid: 'Home', text: 'Home',
+    })
+
+    changeDom.createHtmlElement({ 
+      tag: 'li', parentElement: navlist, elementid: 'Menu', text: 'Menu',
+    })
+
+    changeDom.createHtmlElement({ 
+      tag: 'li', parentElement: navlist, elementid: 'Contact', text: 'Contant',
+    })
 }
 
   export default displaylayout
