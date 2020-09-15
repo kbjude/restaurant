c95b6ecba0cf3725542bdea1049dc1f5ef21ab1b
Jude Kajura <kbjude@gmail.com>
2020-09-15 03:08:35 +0300

    Starting the design

--- src/landingpage.js
+++ src/landingpage.js
@@ -3,6 +3,8 @@ import changeDom from './helpers';
 
 const landingpage = () => {
     const content = document.getElementById('content');
+    const mainsection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['main', 'd-flex']});
+    
 }
 
 export default landingpage;
