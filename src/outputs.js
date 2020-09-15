cb6870c86f51465080c64bf98553e1c0eae97de1
Jude Kajura <kbjude@gmail.com>
2020-09-15 20:34:54 +0300

    Adding elements in the displaylayout function

--- src/outputs.js
+++ src/outputs.js
@@ -1,11 +1,17 @@
 import changeDom from './helpers';
+import { head } from 'lodash';
 
 
 const displaylayout = () => {
     const content = document.getElementById('content');
-    const mainsection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['main']});
-    const middlesection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['middle','d-flex']});
-    const footersection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['footer']});
-  }
+    const header = changeDom.createHtmlElement({tag: 'header', parentElement: 'content', arrayClassName: ['header']});
+    const navbar = changeDom.createHtmlElement({tag: 'nav', parentElement: 'header', arrayClassName: ['navbar']})
+    const mainsection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['mainsection']});
+    const middlesection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['middlesection','d-flex']});
+    const lowersection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['lowersection']});
+    const footersection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['footersection']});
+    const navlist = changeDom.createHtmlElement({tag: 'ul', parentElement: 'navbar', arrayClassName: ['navlist'] })
+    changeDom.createHtmlElement({ })
+}
 
   export default displaylayout
