1d81fd79b11fb8818b0be78885e3c2779f518500
Jude Kajura <kbjude@gmail.com>
2020-09-15 19:57:12 +0300

    Working on the layout as the output

--- src/landingpage.js
+++ src/landingpage.js
@@ -3,7 +3,7 @@ import changeDom from './helpers';
 
 const landingpage = () => {
     const content = document.getElementById('content');
-    const mainsection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['main', 'd-flex']});
+
     
 }
 
--- src/outputs.js
+++ src/outputs.js
@@ -1,6 +1,11 @@
 import changeDom from './helpers';
+
+
 const displaylayout = () => {
     const content = document.getElementById('content');
+    const mainsection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['main']});
+    const middlesection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['middle','d-flex']});
+    const footersection = changeDom.createHtmlElement({tag: 'div', parentElement: 'content', arrayClassName: ['footer']});
   }
 
   export default displaylayout
