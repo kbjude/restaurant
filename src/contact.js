1f655a35676de7752e81525ad6201d3e7ffe1d20
Jude Kajura <kbjude@gmail.com>
2020-09-18 23:17:25 +0300

    Styling the pages

--- src/contact.js
+++ src/contact.js
@@ -1,12 +1,22 @@
+import logo from './images/logo.png';
+
 const contact = () => {
     const contactDiv = document.createElement('div');
-    const contactContent = document.createElement('p');
+    const logoContent = document.createElement('div');
+    const contactContent = document.createElement('div');
     contactContent.innerHTML = "These are our contacts: Phone Number: 07038920832 Email: kbjude@gmail.com";
+
+    const contactLogo = document.createElement('img');
+    contactLogo.src = logo;
+    contactLogo.className = "contactLogo";
 
     contactDiv.appendChild(contactContent);
     const container = document.getElementById('container');
 
     container.innerHTML = '';
     container.appendChild(contactDiv);
+    container.appendChild(logoContent);
+    logoContent.appendChild(contactLogo);
+    logoContent.appendChild(contactContent);
 }
 export default contact;
