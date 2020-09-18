1eb3cc8da201ce6779d8608a7b8742c2d64726d0
Jude Kajura <kbjude@gmail.com>
2020-09-18 21:08:08 +0300

    Adding Logo

--- src/home.js
+++ src/home.js
@@ -1,17 +1,24 @@
 import hotel from './images/background.png';
+import logo from './images/logo.png';
 const home = () => {
     const container = document.getElementById('container');
 
+    const headerDiv = document.createElement('div')
     const header = document.createElement('h1');
     header.innerHTML = "Welcome to The Food Hub";
     header.className = "heading"
+
+    const logoImg = document.createElement('img');
+    logoImg.src = logo;
+    logoImg.className = "logo";
 
     const img = document.createElement('img');
     img.src = hotel;
     img.className = "background-img";
 
     container.innerHTML = '';
-    container.appendChild(header);
+    container.appendChild(headerDiv);
+    headerDiv.appendChild(header);
     container.appendChild(img);
 }
 
