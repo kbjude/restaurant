c3c4a5e7bb33760ada210de676dda1743a1bd342
Jude Kajura <kbjude@gmail.com>
2020-09-19 14:32:09 +0300

    Adding Images

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,13 @@
+import menuItem1 from './images/yummy.png';
+import menuItem2 from './images/yummychicken.png';
+import menuItem3 from './images/salad.png';
+
 const menu = () => {
     const menuDiv = document.createElement('div');
     menuDiv.innerHTML = "Coming Soon";
     const container = document.getElementById('container');
+
+    
 
     container.innerHTML = '';
     container.appendChild(menuDiv)
