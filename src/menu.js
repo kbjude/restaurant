0c26c46b3d214aad0d66a64e639e7ecfc36f2148
Jude Kajura <kbjude@gmail.com>
2020-09-19 15:38:56 +0300

    Adding one row of images on menu

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,7 @@
 import menuItem1 from './images/yummy.png';
 import menuItem2 from './images/yummychicken.png';
 import menuItem3 from './images/salad.png';
+import menuItem4 from './images/dinnertables.jpg';
 
 const menu = () => {
     const menuDiv = document.createElement('div');
@@ -9,10 +10,23 @@ const menu = () => {
 
     const itemsContainer = document.createElement('div');
     const menuImage = document.createElement('img');
-    
+    menuImage.className = "menu-image";
+    menuImage.src = menuItem1;
+
+    const menuImage1 = document.createElement('img');
+    menuImage1.className = "menu-image";
+    menuImage1.src = menuItem2;
+
+    const menuImage2 = document.createElement('img');
+    menuImage2.className = "menu-image";
+    menuImage2.src = menuItem4;
 
     container.innerHTML = '';
-    container.appendChild(menuDiv)
+    container.appendChild(menuDiv);
+    menuDiv.appendChild(itemsContainer);
+    itemsContainer.appendChild(menuImage);
+    itemsContainer.appendChild(menuImage1);
+    itemsContainer.appendChild(menuImage2);
 }
 
 export default menu
