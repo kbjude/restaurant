df9224fe9cadfd3a5e6abc0054272b217756a6f8
Jude Kajura <kbjude@gmail.com>
2020-09-21 18:40:20 +0300

    Setting up the linters

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import home from "./home";
-import initialLoad from "./initialload";
-import contact from "./contact";
-import menu from "./menu";
+import home from './home';
+import initialLoad from './initialload';
+import contact from './contact';
+import menu from './menu';
 
 const onload = () => {
-    initialLoad();
-    home();
-    const mainDiv = document.getElementById('content');
+  initialLoad();
+  home();
+  const mainDiv = document.getElementById('content');
 
-    mainDiv.addEventListener('tabContact', (e) => { 
-        contact();
-    });
-    mainDiv.addEventListener('tabMenu', (e) => { 
-        menu();
-    });
-    mainDiv.addEventListener('tabHome', (e) => { 
-        home();
-    });
-}
+  mainDiv.addEventListener('tabContact', (e) => {
+    contact();
+  });
+  mainDiv.addEventListener('tabMenu', (e) => {
+    menu();
+  });
+  mainDiv.addEventListener('tabHome', (e) => {
+    home();
+  });
+};
 
 window.onload = onload;
